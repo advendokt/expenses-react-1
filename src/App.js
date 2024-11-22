b439797c1e9e2c68ed8ed50738ebc765e0f09a0a
Maksim <a2vend0kt@gmail.com>
2024-11-22 14:11:11 +0200

    Added ID for expenses.id

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Expenses from './components/Expenses';
 const App = () => {
   const expenses = [
     {
-      
+      id: 'e1',
       date: new Date(2024, 10 ,12),
       title:'New book',
       price: 30.99
     },
     {
-
+      id: 'e2',
       date: new Date(2024, 10, 13),
       title: 'Car insurance',
       price: 294.67
